d9bb87846290f0915cbede75485aed9d8ab29ff1
roberto.loaeza <roberto.loaeza@gmail.com>
2024-07-10 06:33:58 +0000

    Actualizando Gradle

---
+++ android/app/src/main/kotlin/com/example/cidr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cidr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/mas_aplicaciones/cidr/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mas_aplicaciones.cidr
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
